f7bef57589395c971439fe1d45e253afa9f5c8fd
Leonel Oliveira <leoneloliver@gmail.com>
2017-10-06 10:38:44 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,50 @@
 // Code goes here
 
+$(document).ready(function() {
 
-$( document ).ready(function() {
+  function Arrows(){
+    function moveL(){
+      var leftPos = $('.div-master').scrollLeft();   
+        $(".div-master").animate({
+            scrollLeft: leftPos - 500
+        }, 800);
+    }
+    
+    function moveR(){
+      var leftPos = $('.div-master').scrollLeft();
+        $(".div-master").animate({
+            scrollLeft: leftPos + 500
+        }, 800);
+    }
 
-  function moveL(){
-    var leftPos = $('.div-master').scrollLeft();   
-      $(".div-master").animate({
-          scrollLeft: leftPos - 162
-      }, 800);
+    $('.before').on('click', function(){
+      moveL();
+    });
+    $('.after').on('click', function(){
+      moveR();
+    });
   }
-
-  function moveR(){
-    var leftPos = $('.div-master').scrollLeft();
-      $(".div-master").animate({
-          scrollLeft: leftPos + 162
-      }, 800);
-  }
-
-  $('.before').on('click', function(){
-    moveL();
-  });
-  $('.after').on('click', function(){
-    moveR()
-  });
 
   function showImages(){
     $(".item-c img").each(function(index) {
       var times = (index*100);
       setTimeout(function(){ $(".item-c img").eq(index).addClass("show-it"); }, times);
     });
+    
   }
-
+  function showList(){
+    $(".item-list img").each(function(index) {
+      var times = (index*120);
+      setTimeout(function(){ $(".item-list img").eq(index).addClass("show-it"); }, times);
+    });
+  }
   setTimeout(function(){
     showImages();
+    Arrows();
   },1000);
-  
-  
-  
+  setTimeout(function(){
+    showList();
+    Arrows();
+  },2000);
   
 });
